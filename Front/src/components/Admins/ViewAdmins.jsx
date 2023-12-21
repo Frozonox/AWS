7414406700dbdcde02a0d46bb51135c939e202d3
Buferxon <buferxon@gmail.com>
2023-12-21 10:20:47 -0500

    Filtross

--- Front/src/components/Admins/ViewAdmins.jsx
+++ Front/src/components/Admins/ViewAdmins.jsx
@@ -5,7 +5,10 @@ function ViewAdmins() {
 	const [cc, setCc] = useState("");
 	const [results, setResults] = useState([]);
 	const [nuser, setNuser] = useState("");
-
+	const [names, setNames] = useState("");
+	const [last_names, setLast_names] = useState("");
+	const [numero, setNumero] = useState("");
+	const [estado, setEstado] = useState("");
 	const getAdmins = async () => {
 		try {
 			const traer = await fetch("http://localhost:8080/users");
@@ -36,12 +39,39 @@ function ViewAdmins() {
 			);
 		}
 
+		if (names) {
+			filteredUsers = filteredUsers.filter((dato) => dato.name === names);
+		}
+		if (last_names) {
+			filteredUsers = filteredUsers.filter(
+				(dato) => dato.last_name === last_names
+			);
+		}
+		if (numero) {
+			filteredUsers = filteredUsers.filter((dato) => dato.movil === numero);
+		}
+		if (estado) {
+			// Aquí debes usar '==' o '===' para comparar, no '='
+			if (estado === "activo") {
+				filteredUsers = filteredUsers.filter(
+					(dato) => dato.status === "ACTIVE"
+				);
+			} else {
+				filteredUsers = filteredUsers.filter(
+					(dato) => dato.status === "INACTIVE"
+				);
+			}
+		}
 		setResults(filteredUsers);
 	}
 
 	function limpiar() {
 		setNuser("");
 		setCc("");
+		setLast_names("");
+		setNames("");
+		setNumero("");
+		setEstado("");
 		setResults(users);
 	}
 
@@ -67,6 +97,44 @@ function ViewAdmins() {
 				placeholder="Por Nombre de Usuario"
 				className="form-control"
 			/>
+			<input
+				value={names}
+				onChange={(e) => {
+					setNames(e.target.value);
+				}}
+				type="text"
+				placeholder="Por Nombre"
+				className="form-control"
+			/>
+			<input
+				value={last_names}
+				onChange={(e) => {
+					setLast_names(e.target.value);
+				}}
+				type="text"
+				placeholder="Por Apellidos	"
+				className="form-control"
+			/>
+			<input
+				value={numero}
+				onChange={(e) => {
+					setNumero(e.target.value);
+				}}
+				type="text"
+				placeholder="Por Numero Telefonico	"
+				className="form-control"
+			/>
+			<select
+				value={estado}
+				onChange={(e) => {
+					setEstado(e.target.value);
+				}}
+				className="form-control"
+			>
+				<option value="">Todos</option>
+				<option value="activo">Activo</option>
+				<option value="inactivo">Inactivo</option>
+			</select>
 			<button onClick={buscar}>Buscar</button>
 			<button onClick={limpiar}>Limpiar</button>
 			<table className="table table-dark table-striped table-bordered mt-5 text-center">
